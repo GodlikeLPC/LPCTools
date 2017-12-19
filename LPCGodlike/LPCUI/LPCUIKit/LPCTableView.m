2137947609d1094932be355c2166184583c897a6
李沛池 <godlikelpc@126.com>
2017-12-19 17:09:56 +0800

    解决iOS11+ VC中的tableView向下移动部分距离

--- LPCGodlike/LPCUI/LPCUIKit/LPCTableView.m
+++ LPCGodlike/LPCUI/LPCUIKit/LPCTableView.m
@@ -10,5 +10,47 @@
 
 @implementation LPCTableView
 
+- (void)configUI
+{
+    if (@available(iOS 11.0, *)) { //解决VC中的tableView向下移动部分距离
+        UIScrollView.appearance.contentInsetAdjustmentBehavior = UIScrollViewContentInsetAdjustmentNever;
+    }
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        [self configUI];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self configUI];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style
+{
+    self = [super initWithFrame:frame style:style];
+    if (self) {
+        [self configUI];
+    }
+    return self;
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self configUI];
+    }
+    return self;
+}
 
 @end
